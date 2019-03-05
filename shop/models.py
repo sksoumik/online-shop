18c02d3ce8722de4d6a9762edee635ab874f0da0
sksoumik <sadmanks@gmail.com>
2019-03-05 17:02:30 +0600

    Templates file

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200,db_index=True)
@@ -12,6 +14,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -31,3 +36,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
